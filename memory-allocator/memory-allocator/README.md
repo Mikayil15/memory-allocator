bf758abb121fc6e60934275b167829fadb7347e6
Mikayil15 <115498560+Mikayil15@users.noreply.github.com>
2026-06-22 16:09:39 +0400

    Add files via upload

---
+++ memory-allocator/memory-allocator/README.md
@@ -0,0 +1,16 @@
+# Memory Allocator in C++
+
+A custom memory allocator implemented in C++ to understand how malloc/free works internally.
+
+## Features
+- Memory pool allocation
+- Custom malloc-like allocate()
+- free() implementation
+- Block header metadata system
+
+## Build
+
+```bash
+cmake .
+make
+./app
---
+++ memory-allocator/memory-allocator/src/MemoryAllocator.cpp
@@ -0,0 +1,66 @@
+//
+// Created by Zeynalabdinov Mikayil on 20.06.26.
+//
+
+#include "../include/allocator/MemoryAllocator.h"
+
+
+#include <iostream>
+#include <cstddef>
+
+MemoryAllocator::MemoryAllocator(size_t size)
+    : pool(size)
+{
+    head = (BlockHeader*)pool.getMemory();
+
+    head->size = size - sizeof(BlockHeader);
+    head->free = true;
+    head->next = nullptr;
+}
+
+void* MemoryAllocator::allocate(size_t size)
+{
+    BlockHeader* current = head;
+
+    while (current)
+    {
+        if (current->free && current->size >= size)
+        {
+            current->free = false;
+
+            return (void*)((char*)current + sizeof(BlockHeader));
+        }
+
+        current = current->next;
+    }
+
+    return nullptr;
+}
+
+
+void MemoryAllocator::deallocate(void* ptr)
+{
+    if (!ptr) return;
+
+    BlockHeader* block =
+        (BlockHeader*)((char*)ptr - sizeof(BlockHeader));
+
+    block->free = true;
+}
+
+
+void MemoryAllocator::printMemory()
+{
+    BlockHeader* current = head;
+    int index = 0;
+
+    while (current)
+    {
+        std::cout << "Block " << index++ << "\n";
+        std::cout << "Size: " << current->size << "\n";
+        std::cout << "Free: " << current->free << "\n";
+        std::cout << "-------------------\n";
+
+        current = current->next;
+    }
+}
